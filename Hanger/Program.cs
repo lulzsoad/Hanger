445f6037b68a770111e06527f94f42b2bfcb2262
Łukasz Łopata <lukasz.lopata96@gmail.com>
2020-03-04 23:19:46 +0100

    Created first database of words and drawn hanger

--- Hanger/Program.cs
+++ Hanger/Program.cs
@@ -4,9 +4,40 @@ namespace Hanger
 {
     class Program
     {
+        /// <summary>
+        /// Source code
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] words = WordsDatabase();
+            string[] hanger = Hanger();
+
+            
+        }
+        /// <summary>
+        /// Returns table of words used in the game.
+        /// </summary>
+        /// <returns></returns>
+        static string[] WordsDatabase()
+        {
+            string[] wordsDatabase = {"airplane", "doctor", "animal", "philosophy", "atmosphere"};
+            return wordsDatabase;
+        }
+        /// <summary>
+        /// Returns drawn hanger
+        /// </summary>
+        /// <returns></returns>
+        static string[] Hanger()
+        {
+            string[] hanger = { " _______",
+                                " |     |",
+                                " |     O",
+                                " |    /|\\",
+                                " |    / \\",
+                                "/|\\      "};
+
+            return hanger;
         }
     }
 }
